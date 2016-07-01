225cb715782a6f4b0912cbe5dd36dd745cab152e
Kai <git@holzinger-it.de>
2016-07-01 09:17:56 +0200

    License Header, minor code improvement
    Changed internal stuff to private
    Added license header

--- src/tracker/Tracker.java
+++ src/tracker/Tracker.java
@@ -1,32 +1,45 @@
 /*
-*Measures time spent in a certain phase. 
-*When the phase is ended, writes the time (in seconds) to a textfile.
-*
-*Call constructor to start, nextPhase() to change to the next phase, 
-*greenBack() to go back from GREEN to RED.
-*Time will be measured and written automatically.
-*/
+ * Copyright (c) 2016. Caro Jachmann, Dominik Kuhnen, Jule Pohlmann, Kai Brandt, Kai Holzinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package tracker;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class Tracker implements TrackerInterface {
+    /*
+     *Measures time spent in a certain phase.
+     *When the phase is ended, writes the time (in seconds) to a textfile.
+     *
+     *Call constructor to start, nextPhase() to change to the next phase,
+     *greenBack() to go back from GREEN to RED.
+     *Time will be measured and written automatically.
+     */
     //seconds spent in each phase so far
-    int greenSeconds;
-    int redSeconds; 
-    int refactorSeconds;
+    private int greenSeconds;
+    private int redSeconds;
+    private int refactorSeconds;
     
     //system time when phase is entered
-    long greenStartTime;
-    long redStartTime;
-    long refactorStartTime;
+    private long greenStartTime;
+    private long redStartTime;
+    private long refactorStartTime;
     
     //current phase (0 = RED, 1 = GREEN, 2 = REFACTOR)
-    int phase;
+    private int phase;
            
     public Tracker() {
-        //initialise times to 0
+        //initialize times to 0
         this.greenSeconds = 0;
         this.redSeconds = 0;
         this.refactorSeconds = 0;
@@ -34,7 +47,7 @@ public class Tracker implements TrackerInterface {
         //start in RED phase
         this.phase = 0;
         
-        redStartTime = System.currentTimeMillis();
+        this.redStartTime = System.currentTimeMillis();
     }
     
     //change to next phase, automatically updates phase
@@ -61,30 +74,28 @@ public class Tracker implements TrackerInterface {
         }
     }
 
-    //following methods are only used by this class and should not 
-    //be called outside of it
-     
-    //@Override
-    public void redToGreen() {
+                                        /*INTERNAL METHODS*/
+
+    private void redToGreen() {
         long redEndTime = System.currentTimeMillis() - redStartTime;
         redSeconds += (int)redEndTime/1000;
         greenStartTime = System.currentTimeMillis();
     }
     
-    public void greenToRefactor() {
+    private void greenToRefactor() {
         long greenEndTime = System.currentTimeMillis() - greenStartTime;
         greenSeconds += (int)greenEndTime/1000;
         refactorStartTime = System.currentTimeMillis();
     }
     
-    public void refactorToRed() {
+    private void refactorToRed() {
         long refactorEndTime = System.currentTimeMillis() - refactorStartTime;
         refactorSeconds += (int)refactorEndTime/1000;
         redStartTime = System.currentTimeMillis();
     }
 
     //write to file:
-    public void writeToFile() {
+    private void writeToFile() {
         try (PrintWriter printFile = new PrintWriter("TrackingData.txt")) {
             printFile.println("red\n" + redSeconds);
 	    printFile.println("green\n" + greenSeconds);
