8d3c3f43d4cab229deb152ebe1b4a39b7c8b3434
Kai <git@holzinger-it.de>
2016-07-01 09:16:44 +0200

    Header, minor code cleanup / -improvements
    Added License Header
    Changed some code for better readability
    Change stage size to make it fit to the scene
    Added some fancy icon
    Changed BG color from white to some grey-ish color (easier to look at)
    Changed round from 3 to 1 decimal place

--- src/Chart/TrackingChart.java
+++ src/Chart/TrackingChart.java
@@ -1,8 +1,23 @@
-package projekt7;
+/*
+ * Copyright (c) 2016. Caro Jachmann, Dominik Kuhnen, Jule Pohlmann, Kai Brandt, Kai Holzinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+package Chart;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -10,47 +25,43 @@ import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class TrackingChart extends Application implements Chart {
-	
+
+	//Prepare the stage
     @Override 
     public void start(Stage stage) {
-	     stage.setTitle("Tracking Analyse");
-	     stage.setWidth(520);
-	     stage.setHeight(500);
-		 stage.setScene(erstelleScene());
-	     stage.show();		
+		stage.setTitle("Tracking Analyse");
+		stage.getIcons().add(new Image("file:icon.png"));
+		stage.sizeToScene();
+		stage.setScene(erstelleScene());
+		stage.show();
     }
-    	
+
+	/*
+	* @return int containing data gathered from the tracker. Reads result from a file.
+	* @param info:
+	* 1: returns value for green,
+	* 2: returns value for red,
+	* 3: returns value for refactor,
+	* any other value returns 0.
+	* */
 	@Override
-	public int leseZeiten(int info) {
-		try(BufferedReader read = new BufferedReader(new FileReader("TrackingData.txt"))){
-			read.readLine();
-			String green = read.readLine();
-			read.readLine();
-			String red = read.readLine();
-			read.readLine();
-			String refactor = read.readLine();
-		
-			int gre = Integer.parseInt(green);
-			int re  = Integer.parseInt(red);
-			int ref = Integer.parseInt(refactor);
-		
-			if(info == 1) return gre;
-			if(info == 2) return re;
-			if(info == 3) return ref;
-						
-			read.close();
-		}
-			catch (IOException e){
-				e.printStackTrace();
-			}
-		return 0;
+	public int leseZeiten(int info) throws IOException {
+		List<String> list = Files.readAllLines(Paths.get("TrackingData.txt"));
+
+		return (info == 1) ? (Integer.parseInt(list.get(3)))
+				: (info == 2) ? Integer.parseInt(list.get(1))
+				: (info == 3) ? Integer.parseInt(list.get(5))
+				: 0;
+
 	}
-		
+
+	//@return a PieChart Element which represents how much time it took the user to finish the specific exercise-stage
 	@Override
-	public PieChart erstellePieChart() {
+	public PieChart erstellePieChart() throws IOException {
 		ObservableList<PieChart.Data> daten = FXCollections.observableArrayList(
                 new PieChart.Data("Red " + leseZeiten(1) + " Sekunden", leseZeiten(1)),                	
                 new PieChart.Data("Green " + leseZeiten(2) + " Sekunden", leseZeiten(2)),	                
@@ -58,42 +69,40 @@ public class TrackingChart extends Application implements Chart {
         PieChart pieChart = new PieChart(daten);
         pieChart.setTitle(setzteTitel());
         pieChart.setLegendVisible(false);
-       
-        int i = 0;        
-        for (PieChart.Data data : daten) {
-        	if(i == 0) data.getNode().setStyle("-fx-pie-color: #FF0000;");
-        	if(i == 1) data.getNode().setStyle("-fx-pie-color: #008000;");
-        	if(i == 2) data.getNode().setStyle("-fx-pie-color: #4682B4;");
-            i++;
-        }         
+		pieChart.setStyle("-fx-background-color: rgba(4, 6, 4, 0.04)");
+
+		for (int i = 0; i < daten.size(); i++) {
+			if (i == 0) daten.get(i).getNode().setStyle("-fx-pie-color: #FF0000;");
+			if (i == 1) daten.get(i).getNode().setStyle("-fx-pie-color: #008000;");
+			if (i == 2) daten.get(i).getNode().setStyle("-fx-pie-color: #4682B4;");
+		}
         return pieChart;
 	}
-	
-	@Override
-	public String setzteTitel() {
+
+	public String setzteTitel() throws IOException{
 		double red = leseZeiten(1);
 		double green = leseZeiten(2);
 		double refactor = leseZeiten(3);
-		double übergang = red + green + refactor;
-		double prozent = 100 / übergang;
+		double uebergang = red + green + refactor;
+		double prozent = 100 / uebergang;
 		red = red * prozent;
-		red = Math.round(red * 1000)/1000.0;
+		red = Math.round(red * 10)/10.0;
 		green = green * prozent;
-		green = Math.round(green * 1000)/1000.0;
+		green = Math.round(green * 10)/10.0;
 		refactor = refactor * prozent;
-		refactor = Math.round(refactor * 1000)/1000.0;
-		
-		String ausgabe = ("Sie haben " + red + "% ihrer Zeit in der Phase RED verbracht, \n                 "
+		refactor = Math.round(refactor * 10)/10.0;
+
+		return ("Sie haben " + red + "% ihrer Zeit in der Phase RED, \n                 "
 		+ green + "% in der Phase GREEN und \n                 "
-		+ refactor +"% in der REFACTOR-Phase.");
-		
-		return ausgabe;
+		+ refactor +"% in der REFACTOR-Phase verbracht.");
 	}
 	
-	@Override
+	//@return a scene which contains a PieChart child
 	public Scene erstelleScene() {
 		Scene scene = new Scene(new Group());
-        ((Group) scene.getRoot()).getChildren().addAll(erstellePieChart());       
+		try {
+			((Group) scene.getRoot()).getChildren().addAll(erstellePieChart());
+		} catch (IOException e) {}
 		return scene;
 	}
 
